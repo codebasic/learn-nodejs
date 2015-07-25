6859423d87c3709fc5044837046b9a74764563a0
이성주 <seongjoo.lee@gmail.com>
2015-07-25 11:35:00 +0900

    예시 추가

--- inheritance/challenges/jedi-constructor-inherit.js
+++ inheritance/challenges/jedi-constructor-inherit.js
@@ -1,6 +1,42 @@
-function Jedi(familyname, name){
+/*
+도전과제
+
+출력예시:
+
+스카이워커 아나킨
+사악한 포스 사용
+정신력이 약한 사람에게만 적용된다.
+사악한 속임수 쓰기
+
+오비완 카노비
+포스 사용
+정신력이 약한 사람에게만 적용된다.
+useDeception를 사용할 수 없습니다.
+
+*/
+
+function Jedi(familyname, name, force){
   this.familyname = familyname
   this.name = name;
+
+  Object.defineProperty(this, "force", {
+    get: function(){
+      console.log(this._force + ' 포스가 느껴진다.');
+      return this._force;
+    },
+    set: function(value){
+      value = parseFloat(value);
+      if(value < 0){
+        throw new Error('포스는 0보다 커야 합니다.');
+      }
+      console.log('포스가 함께하길');
+
+      this._force = value;
+    },
+    enumerable: true
+  });
+
+  this.force = force;
 }
 
 Jedi.prototype.sayName = function(){
@@ -16,8 +52,8 @@ Jedi.prototype.controlMind = function(){
   console.log('정신력이 약한 사람에게만 적용된다.');
 }
 
-function DarkJedi(familyname, name){
-  Jedi.call(this, familyname, name);
+function DarkJedi(familyname, name, force){
+  Jedi.call(this, familyname, name, force);
 }
 
 // 프로토타입 상속
@@ -34,10 +70,8 @@ DarkJedi.prototype.useForce = function(){
 }
 
 
-var 다스베이더 = new DarkJedi('스카이워커', '아나킨');
-var 오비완  = new Jedi('오비완', '카노비');
-
 function doJedi(jedi){
+  jedi.force;
   var 능력 = ['sayName', 'useForce', 'controlMind', 'useDeception'];
   for(var i=0; i<능력.length; i++){
     if(!(능력[i] in jedi)){
@@ -46,7 +80,13 @@ function doJedi(jedi){
     }
     jedi[능력[i]]();
   }
+  console.log();
 }
 
+var 다스베이더 = new DarkJedi('스카이워커', '아나킨', 10000);
 doJedi(다스베이더);
+
+var 오비완  = new Jedi('오비완', '카노비', 2000);
 doJedi(오비완);
+
+var 이성주 = new Jedi('이', '성주', -1);
