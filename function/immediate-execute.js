f4d510b26131be90ad1d068c8ebc45c120785602
이성주 <seongjoo@codebasic.co>
2016-07-05 13:30:00 +0900

    파일명 변경

--- function/immediate-execute.js
+++ function/immediate-execute.js
@@ -1,17 +1,21 @@
+var assert = require('assert');
+
 // 함수 정의 즉시 실행
 // 함수를 실행하고 나면, 변수에 할당되지 않아 다시 실행할 수 없음.
 (function foo(){
     console.log('실행됐습니다!');
 })();
 
-foo(); // ReferenceError! 함수 정의가 더이상 유효하지 않음.
+// ReferenceError! 함수 정의가 더이상 유효하지 않음.
+assert.throws(()=>{foo()}, ReferenceError);
 
 // {}는 새로운 스코프를 생성하지 않는다.
 var x=123;
 if(true){
-    (function(){
-        var x=456; // 새로운 스코프
-        console.log('함수 내 x값:' + x);
-    })();
+  (function(){
+      var x=456; // 새로운 스코프
+      assert(x, 456);
+  })();
 }
-console.log(x);
+
+assert(x, 123)
