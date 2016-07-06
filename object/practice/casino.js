f2892596ab45e3f8e1db17ad48848445d64dc7e2
Lee Seongjoo <seongjoo@codebasic.co>
2016-07-06 12:30:41 +0900

    예시추가

--- object/practice/casino.js
+++ object/practice/casino.js
@@ -1,4 +1,4 @@
-module.exports.환전 = function(화폐, 대상통화){
+function 환전(화폐, 대상통화){
   var 환율기준 = {'USD': 1100, 'KRW':1};
 
   // 환전 가능 화폐
@@ -12,3 +12,9 @@ module.exports.환전 = function(화폐, 대상통화){
   var 환전화폐 = 화폐.금액 * 환율;
   return 환전화폐;
 };
+
+module.exports.환전 = 환전;
+module.exports.달러로얼마 = function(){
+  var 환전금액 = 환전(this.자본, 'USD');
+  return 환전금액;
+}
