67b8618cab33d9cb637a5a6f7df35d2b43468629
Lee Seongjoo <seongjoo@codebasic.co>
2016-07-07 12:23:04 +0900

    refactored

--- web/http/restful/restful.js
+++ web/http/restful/restful.js
@@ -1,93 +1,98 @@
 var http = require('http');
 var url = require('url');
-var items = [];
 
-var server = http.createServer(function(req, res){
-  switch(req.method){
-    case 'POST':
-      var item = '';
-      req.setEncoding('utf-8');
-      // 데이터 스트림 수신 이벤트 발생 시
-      req.on('data', function(chunk){
-        item += chunk;
-      });
-      // 전체 데이터 수신 완료 시
-      req.on('end', function(){
-        items.push(item);
-        res.end('OK\n');
-      });
-      break;
-    case 'GET':
-      var body = items.map(function(item, i){
-        return i+ ') ' + item;
-      }).join('\n');
-      body += '\n';
+var items = []; // 인메모리 "DB"
 
-      // 문자열의 길이가 아닌 바이트 크기를 설정
-      // 한글은 한 글자가 3바이트
-      var bytes = Buffer.byteLength(body);
-      res.setHeader('Content-Length', bytes);
-      res.setHeader('Content-Type',
-        'text/plain;charset="utf-8"');
+function 색인확인(요청, 응답){
 
-      res.end(body);
-      break;
-    case 'DELETE':
-      deleteItem(req, res);
-      break;
-    case 'PUT':
-      updateItem(req, res);
-      break;
+}
+
+function 항목등록(요청, 응답){
+  var 항목 = '';
+  요청.setEncoding('utf8');
+  요청.on('data', function(조각){
+    항목 += 조각;
+  });
+  요청.on('end', function(){
+    items.push(항목);
+    console.log(items);
+    응답.end('OK\n');
+  });
+}
+
+function 항목내역출력(요청, 응답){
+  응답.setHeader('Content-Type', 'text/plain;charset=utf8');
+
+  items.forEach(function(항목, 색인){
+    var 항목형식 = 색인 +') ' + 항목 + '\n';
+    응답.write(항목형식);
+  });
+
+  응답.end();
+}
+
+function 항목삭제(요청, 응답){
+  var path = url.parse(요청.url).pathname;
+  var 색인 = parseInt(path.slice(1), 10);
+
+  if(isNaN(색인)){
+    응답.statusCode = 400;
+    응답.end('Invalid index: ' + 색인);
+  } else if(!items[색인]){
+    // 색인이 번호이긴 하지만, 해당되는 항목이 없는 경우
+    응답.statusCode = 404;
+    응답.end('Item not found at ' + 색인);
+  } else{
+    items.splice(색인, 1); // 색인 위치 항목 삭제
+    응답.end('OK\n');
   }
+}
+
+function 항목갱신(요청, 응답){
+  var path = url.parse(요청.url).pathname;
+  var 색인 = parseInt(path.slice(1), 10);
+
+  if(isNaN(색인)){
+    응답.statusCode = 400;
+    응답.end('Invalid index: ' + 색인);
+  } else if(!items[색인]){
+    // 색인이 번호이긴 하지만, 해당되는 항목이 없는 경우
+    응답.statusCode = 404;
+    응답.end('Item not found at ' + 색인);
+  } else{
+    var 항목 = '';
+    요청.on('data', function(조각){
+      항목 += 조각;
+    });
+    요청.on('end', function(){
+      items[색인] = 항목;
+      응답.end('OK\n');
+    });
+  }
+}
+
+var server = http.createServer(function(요청, 응답){
+  var 요청처리 = {
+    POST: function(){
+      console.log('POST 요청 수신');
+      항목등록(요청, 응답);
+    },
+    GET: function(){
+      console.log('GET 요청 수신');
+      항목내역출력(요청, 응답);
+    },
+    DELETE: function(){
+      console.log('DELETE 요청 수신');
+      항목삭제(요청, 응답);
+    },
+    PUT: function(){
+      console.log('PUT 요청 수신');
+      항목갱신(요청, 응답);
+    }
+  };
+
+  요청처리[요청.method]();
 });
 
-function deleteItem(req, res){
-  var path = url.parse(req.url).pathname;
-  var i = parseInt(path.slice(1)); // 문자열을 정수 값으로 변환
-
-  if(isNaN(i)){
-    // i가 숫자가 아닌 경우는 오류
-    res.statusCode = 400;
-    res.end('유효하지 않은 아이템 식별자');
-  } else if(!items[i]){
-    // i 번에 해당하는 아이템이 없는 경우
-    res.statusCode = 404; // 리소스를 못 찾는 경우
-    res.end('해당 아이템 없음.');
-  } else{
-    // 해당 식별자에 항목이 존재하는 경우
-    items.splice(i, 1); // i 번째 인덱스부터 1개 값 삭제
-    res.end('OK\n');
-  }
-}
-
-function updateItem(req, res){
-  var path = url.parse(req.url).pathname;
-  var i = parseInt(path.slice(1)); // 문자열을 정수 값으로 변환
-  debugger;
-  
-  if(isNaN(i)){
-    // i가 숫자가 아닌 경우는 오류
-    res.statusCode = 400;
-    res.end('유효하지 않은 아이템 식별자');
-  } else if(!items[i]){
-    // i 번에 해당하는 아이템이 없는 경우
-    res.statusCode = 404; // 리소스를 못 찾는 경우
-    res.end('해당 아이템 없음.');
-  }
-  else{
-    var item = '';
-
-    req.setEncoding('utf-8');
-    req.on('data', function(chunk){
-      item += chunk;
-    });
-    req.on('end', function(){
-      items[i] = item;
-      res.end('OK\n');
-    });
-  }
-
-}
-
 server.listen(3000);
-console.log('서버 실행 중');
+console.log('RESTful 서버 실행 중');
